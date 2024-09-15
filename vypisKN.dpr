53aa0eb6337c5a5c79442bc0f4eb5aee1b84af22
vitasrutek <98422550+vitasrutek@users.noreply.github.com>
2024-09-15 23:14:35 +0200

    Add files via upload
    v2.2-dev

--- vypisKN.dpr
+++ vypisKN.dpr
@@ -4,8 +4,7 @@ uses
   Vcl.Forms,
   Unit1 in 'Unit1.pas' {mainForm},
   Vcl.Themes,
-  Vcl.Styles,
-  Unit2 in 'Unit2.pas' {clearForm};
+  Vcl.Styles;
 
 {$R *.res}
 
@@ -15,6 +14,5 @@ begin
   Application.Title := 'CUZK-scraper';
   TStyleManager.TrySetStyle('Windows11 Modern Light');
   Application.CreateForm(TmainForm, mainForm);
-  Application.CreateForm(TclearForm, clearForm);
   Application.Run;
 end.
